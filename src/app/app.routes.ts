aa72abb7a0ac67a1ceff33dcf9876f39f5289aac
Jakov Glavač <jglavac21@student.foi.hr>
2025-11-12 02:59:08 +0100

    SCRUM-87 Added registration component, added input fields with labels
    I added new registration component to the angular project. Then I designed registration UI which is based on wireframe which I made previously for registration UI.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'register' },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./features/auth/registration/registration')
+        .then(m => m.Registration),
+  },
+];

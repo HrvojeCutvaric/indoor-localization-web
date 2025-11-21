8cba41aacbabde388e9316ccb7733347728e0889
Jakov Glavač <jglavac21@student.foi.hr>
2025-11-21 02:08:38 +0100

    SCRUM-91 Added input fields for first name and last name

--- src/app/features/auth/registration/registration.ts
+++ src/app/features/auth/registration/registration.ts
@@ -32,6 +32,8 @@ export class Registration {
     {
       email: ['', [Validators.required, Validators.email]],
       username: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(24)]],
+      firstName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(30)]],
+      lastName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(30)]],
       password: [
         '',
         [
